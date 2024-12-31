3ab988069665184211151d3e5b950187d43262d2
subproject9 <45178092+subproject9@users.noreply.github.com>
2024-12-31 14:10:53 -0600

    Bug Fix
    Bug Fix

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  server: {
-    host: true,
-    port: 5173,
-    strictPort: true,
-  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    sourcemap: false
   }
 })
