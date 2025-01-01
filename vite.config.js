d70ae76f536520bd7fad14978adcc0bba6f8ebb1
subproject9 <45178092+subproject9@users.noreply.github.com>
2024-12-31 18:14:06 -0600

    Bug Update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,20 @@
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default {
-  plugins: [react()]
-}
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    minify: 'terser',
+    cssMinify: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom'],
+          'animations': ['framer-motion']
+        }
+      }
+    },
+    sourcemap: false,
+    chunkSizeWarningLimit: 1000
+  }
+})
